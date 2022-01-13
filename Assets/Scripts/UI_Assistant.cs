cd3b5345129a073300f0850cceff0bc8dc744f40
Kristian Rolando Limantara <90307240+aldolim66@users.noreply.github.com>
2022-01-13 13:15:48 +0800

    Prolog

---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    [SerializeField] private Text messageText;
+    [SerializeField] private float timeTyping = 0.3f;
+    [SerializeField] private string[] message;
+
+    private int index = 0;
+    
+
+    private void Update()
+    {
+        if(textWriter.statusDone == true)
+        {
+            textWriter.statusDone = false;
+            textWriter.AddWriter(messageText, message[index], timeTyping);
+            index++;
+        }  
+    }
+
+}
